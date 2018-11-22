bf3b212d057a5eab949db0c04ccf0f755a4ae346
Leo Saenger <leosaenger@gmail.com>
2018-11-22 12:36:09 -0600

    getting, reading, and parsing segments (as GPX)

--- helpers.py
+++ helpers.py
@@ -1,26 +1,69 @@
-# import time
-# import swagger_client
-# from swagger_client.rest import ApiException
-# from pprint import pprint
+# Strava modules
+import time
+import swagger_client
+from swagger_client.rest import ApiException
+from pprint import pprint
 import requests
 import urllib.parse
 
+# Flask session control tools
 from flask import redirect, render_template, request, session
 from functools import wraps
 
+# GPX parser, documentation: https://github.com/tkrajina/gpxpy
+import gpxpy
+import gpxpy.gpx
 
-# # Configure OAuth2 access token for authorization: strava_oauth
-# swagger_client.configuration.access_token = 'YOUR_ACCESS_TOKEN'
-#
-# # create an instance of the API class
-# api_instance = swagger_client.RoutesApi()
-# id = 56 # Integer | The identifier of the route.
-#
-# try:
-#     # Export Route GPX
-#     api_instance.getRouteAsGPX(id)
-# except ApiException as e:
-#     print("Exception when calling RoutesApi->getRouteAsGPX: %s\n" % e)
+# Configure OAuth2 access token for authorization: strava_oauth
+swagger_client.configuration.access_token = 'YOUR_ACCESS_TOKEN'
+
+def get_segments(lower, upper):
+    '''Accepts lower and upper bounds for an area, and returns a list of ids for nearby segments'''
+
+    # create an instance of the API class
+    api_instance = swagger_client.SegmentsApi()
+    bounds =  [lower, upper]# array[Float] | The latitude and longitude for two points describing a rectangular boundary for the search: [southwest corner latitutde, southwest corner longitude, northeast corner latitude, northeast corner longitude]
+    activityType = running # Get running routes
+
+    try:
+        # Explore segments
+        segments = api_instance.exploreSegments(bounds, activityType=activityType)
+        # Gets a list of the IDs for segments returned
+        segment_ids = [n["id"] for n in segments]
+        return segment_ids
+    except ApiException as e:
+        print("Exception when calling SegmentsApi->exploreSegments: %s\n" % e)
+
+
+def get_gpx(route_id):
+    '''Takes a route id and returns a GPX file for that route'''
+
+    # create an instance of the API class
+    api_instance = swagger_client.RoutesApi()
+    id = route_id # Integer, the identifier of the route.
+    try:
+        # Export Route GPX
+        gpx_route = api_instance.getRouteAsGPX(id)
+        return gpx_route
+    except ApiException as e:
+        print("Exception when calling RoutesApi->getRouteAsGPX: %s\n" % e)
+
+
+def read_gpx():
+    '''Parses a GPX file and returns a list of coordinates'''
+
+    # Opens the GPX file
+    gpx_file = open(gpx_route, 'r')
+    gpx = gpxpy.parse(gpx_file)
+
+    # Parses a GPX file, as per documentation of gpxpy
+    for track in gpx.tracks:
+    for segment in track.segments:
+        for point in segment.points:
+            # Appends latitude and longitude of each point as a tuple to a list
+            route_coords = []
+            route_coords.append((point.latitude, point.longitude))
+
 
 def login_required(f):
     """
