cbaa9b064bc93b476753d5298c84a9835a87c71a
Nihal Raman <nihalraman@college.harvard.edu>
2018-11-30 01:21:07 -0500

    FUCK THIS SHIT

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
-#from pymsgbox import *
 
 # Configure application
 app = Flask(__name__)
@@ -36,15 +35,13 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///runanywhere.db")
 
-saver = 0
-
 @app.route("/")
 # @login_required
 def landing():
     """Show landing page"""
     return(render_template("index.html"))
 
-@app.route("/get_coords", methods=["GET"])
+@app.route("/get_routes", methods=["GET"])
 def get_coords():
     # Fetch the current latitude and longitude
     currentlat = request.args.get('lat', 0, type=float)
@@ -60,106 +57,12 @@ def get_coords():
     seg_data = {}
     seg_data['segments'] = []
     for n in range((len(data))):
-        seg_data['segments'].append({'name': data[n].name, 'avg_grade': data[n].avg_grade,
-                                    'elev_difference': str(data[n].elev_difference.__dict__['_num']) + str(data[n].elev_difference.__dict__['_unit']),
-                                    'distance': str(data[n].distance.__dict__['_num']) + str(data[n].distance.__dict__['_unit'])})
-    # Save data
-    with open('./routes-data.json', 'w') as file:
-        json.dump(seg_data, file)
-        file.close()
-    # Iterates over the original object returned, taking out polylines
-    polylines = []
-    for n in range(len(data)):
-        polylines.append(data[n].points)
-    # Convert those to a list of coordinates
-    coordinates = [polyline.decode(n) for n in polylines]
+        seg_data['segments'].append({'name': data[n].name,
+                                    'avg_grade': data[n].avg_grade,
+                                    # We have to extract the elevation difference and distance from the returned object
+                                    'elev_difference': str(data[n].elev_difference.__dict__['_num']) + ' ' + str(data[n].elev_difference.__dict__['_unit']),
+                                    'distance': str(data[n].distance.__dict__['_num']) + ' ' + str(data[n].distance.__dict__['_unit']),
+                                    # Points are returned both backwards and coded as a polyline, so we fix that
+                                    'points': flip(polyline.decode(data[n].points))})
     # Then, return those
-    return jsonify(flip(coordinates))
-
-@app.route("/get_routes", methods=["GET"])
-def get_routes():
-    print("called get_routes")
-    # Fetch data from earlier call
-    with open('./routes-data.json', 'r') as file:
-        data = json.load(file)
-        file.close()
-        return jsonify(data)
-
-# This function is a work in progress
-@app.route("/save_route", methods=["GET", "POST"])
-def save_route():
-    """Register user"""
-    rows = db.execute("INSERT INTO routes(route, user_id) users WHERE id = :i", i=session['user_id'])
-
-        # Remember which user has logged in
-    for row in rows:
-        session["user_id"] = row["id"]
-
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user"""
-    if request.method == "POST":
-        # Checking for valid inputs
-        if not request.form.get("username") or not request.form.get("password") or not request.form.get("confirmation"):
-            return apology("Please enter a valid username!")
-        if request.form.get("password") != request.form.get("confirmation"):
-            return apology("Different passwords!")
-        # Hashing the password
-        hash = generate_password_hash(request.form.get("password"))
-        result = db.execute("INSERT INTO users(username, hash) VALUES(:route, :i)",
-                    route = TODO, i = session['user_id'])
-
-        # Redirect user to home page
-        return redirect("/")
-    else:
-        return render_template("register.html")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """Log user in"""
-
-    # Forget any user_id
-    session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            alert("must provide username", title='', button='OK')
-            return 1
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            alert("must provide password", title='', button='OK')
-            return 1
-
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username").lower())
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-
-@app.route("/logout")
-def logout():
-    """Log user out"""
-
-    # Forget any user_id
-    session.clear()
-
-    # Redirect user to login form
-    return redirect("/")
+    return jsonify(seg_data)
